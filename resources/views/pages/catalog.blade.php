c49b6a5d9ce3755064b77e4de4f7fbe045f0ad65
1P-Muhammed <mo7amedsheta033@gmail.com>
2025-04-23 15:52:21 +0200

    addMigratoin

--- resources/views/pages/catalog.blade.php
+++ resources/views/pages/catalog.blade.php
@@ -1,26 +1,33 @@
 @extends('layouts.layout')
 @section('content')
+
     <main>
         <section class="catalog-section">
             <h1 class="catalog-title">Products</h1>
             <div class="catalog-items">
                 <!-- item -->
+                @foreach ($catalogs as $catalog )
+
                 <div class="catalog-item">
                     <div class="catalog-img-container">
-                        <img src="../images/product/1.png" alt="Product 1" />
+                        {{-- @foreach ($images as $image) --}}
+                            {{-- @dd($image) --}}
+                            <img src="{{ $catalog->images }}" alt="Product 1" />
+                        {{-- @endforeach --}}
                         <p class="catalog-img-save">
                             <i class="fa-solid fa-tag"></i> Save LE 451.00
                         </p>
                     </div>
                     <div class="catalog-item-info">
-                        <h2>كرسي اسود طبي مع ذراع ثابت</h2>
+                        <h2> {{ $catalog->name }} </h2>
                         <div class="catalog-price-container">
-                            <p>LE 1,599.00</p>
-                            <p class="price">LE 2,000.00</p>
+                            <p>LE {{ $catalog->discount_price }}</p>
+                            <p class="price">LE {{ $catalog->price }}</p>
                         </div>
                         <button>Buy</button>
                     </div>
                 </div>
+                @endforeach
                 <!-- item end -->
             </div>
         </section>
