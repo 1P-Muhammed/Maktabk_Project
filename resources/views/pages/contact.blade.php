401ab957b5d1457df0e260d0a9d239467798db68
0Migrant0 <towls320@gmail.com>
2025-04-23 18:28:42 +0200

    addContactPage

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.layout')
 @section('content')
+    <main>
+        <!-- Page title -->
+        <section class="contact-header">
+            <div class="container">
+                <h1 class="contact-title">Contact</h1>
+            </div>
+        </section>
 
-
+        <!-- Contact form -->
+        <section class="contact-form-section">
+            <div class="container">
+                <form action="/contact" method="post" class="contact-form">
+                    <div class="form-group-row">
+                        <div class="form-group">
+                            <label for="contact-name">Name *</label>
+                            <input type="text" id="contact-name" name="name" placeholder="Name" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="contact-email">Email *</label>
+                            <input type="email" id="contact-email" name="email" placeholder="Email" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="contact-phone">Phone Number</label>
+                        <input type="tel" id="contact-phone" name="phone" placeholder="Phone Number" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="contact-message">Message *</label>
+                        <textarea id="contact-message" name="message" rows="7" placeholder="Message" required></textarea>
+                    </div>
+                    <button type="submit" class="submit-button">Send &rAarr;</button>
+                </form>
+            </div>
+        </section>
+    </main>
 @endsection
