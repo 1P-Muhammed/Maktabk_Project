d52c2d40d4491db05a3fde1126168fca733eb8f4
1P-Muhammed <mo7amedsheta033@gmail.com>
2025-04-23 22:28:08 +0200

    editOnMigration

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -10,7 +10,7 @@ class CatalogController extends Controller
     public function catalog()
     {
         $data = Catalog::all();
-        $image = Image::all();
+        $image = Catalog::findOrFail('1')->images;
         return view("pages.catalog", [ 'catalogs' => $data, 'images'=> $image ]);
     }
 }
